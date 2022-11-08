71648a19df13aad4fe74db53699ca06cd5c4f6c2
FridoS <frido.schaefer@hochschule-stralsund.de>
2022-11-08 10:47:37 +0100

    baseline stuff

--- src/aufgabe01/Adresse.java
+++ src/aufgabe01/Adresse.java
@@ -1,5 +1,29 @@
 package aufgabe01;
 
-public class Adresse
-{
-}
+import java.io.Serializable;
+  public class Adresse implements Serializable
+  {
+    private String strasse;
+    private String ort;
+
+    public String getStrasse() {
+      return this.strasse;
+    }
+
+    public void setStrasse(String strasse) {
+      this.strasse = strasse;
+    }
+
+    public String getOrt() {
+      return ort;
+    }
+
+    public void setOrt(String ort) {
+      this.ort = ort;
+    }
+
+    public String toString() {
+      return new StringBuilder().append(strasse).append(", ")
+          .append(ort).toString();
+    }
+  }
