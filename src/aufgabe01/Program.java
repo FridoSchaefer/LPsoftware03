7ae353341d5b72e43b1648fb8877f341eb70a95a
FridoS <frido.schaefer@hochschule-stralsund.de>
2022-11-08 11:06:59 +0100

    complete 01

--- src/aufgabe01/Program.java
+++ src/aufgabe01/Program.java
@@ -1,5 +1,8 @@
 package aufgabe01;
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class Program
 {
 
@@ -16,5 +19,42 @@ public class Program
     Person erika = new Person();
     erika.setName("Erika Schmidt");
     erika.setAdresse(adresse);
+
+    //Array List erstellen und befüllen
+    ArrayList<Person> personen = new ArrayList<>();
+    personen.add(erika);
+    personen.add(hugo);
+
+    File test = new File("src\\aufgabe01\\Personen.txt");
+
+    //Serialisierung von Person zu Datei
+    try (FileOutputStream fileOutputStream = new FileOutputStream(test)) {
+      ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+      objectOutputStream.writeObject(personen);
+      objectOutputStream.close();
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+    //neue ArrayList zur Deserialisierung
+    ArrayList<Person> newPersonen = new ArrayList<>();
+
+    //Deserialisierung von Datei zu Objekt
+    try (FileInputStream fileInputStream = new FileInputStream(test)) {
+      ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+      newPersonen = (ArrayList) objectInputStream.readObject();
+      objectInputStream.close();
+    }
+    catch (IOException | ClassNotFoundException e) {
+      e.printStackTrace();
+    }
+
+    //Ausgabe der Personen
+    for (Person person : newPersonen) {
+      System.out.println(person);
+    }
+
+    //Überprüfung auf Gleichheit der Adressen
+    System.out.println(newPersonen.get(0).getAdresse()==(newPersonen.get(1).getAdresse()));
   }
 }
